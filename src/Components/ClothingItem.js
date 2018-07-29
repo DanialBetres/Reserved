993314883e4c835fcdd6d985af70511dadfc12ed
Danial Betres <dbetres@secretmission.io>
2018-07-29 12:22:15 -0400

    dd

--- src/Components/ClothingItem.js
+++ src/Components/ClothingItem.js
@@ -2,7 +2,13 @@ import React from "react";
 
 const ClothingItem = ({itemImage,name,price, storeName}) => {
   return(
-  <div className="clothingItemContainer">
+    <div style={{  
+      display:"flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "flex-end",
+      margin: "11px",
+      marginTop: storeName ? "15px" : "0px"}} >
     {storeName && <p className="storeName">{storeName}</p> }
     <div className="clothingImage" style={{ backgroundImage: `url(${itemImage})` }}
 >
