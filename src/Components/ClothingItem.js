179c0acfe5c7493988e298b335c1dc37614a4f43
Danial Betres <dbetres@secretmission.io>
2018-07-29 10:28:22 -0400

    clothingItems

---
+++ src/Components/ClothingItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ClothingItem = ({itemImage,name,price}) => {
+console.log(itemImage)
+  return(
+  <div className="clothingItemContainer">
+    <div className="clothingImage" style={{ backgroundImage: `url(${itemImage})` }}
+>
+    </div>
+    <p className="clothingName">{name}</p>
+    <p className="clothingPrice">{price}</p>
+  </div>
+  )
+}
+
+export default ClothingItem;
--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,27 +1,28 @@
-import React, {Component } from 'react'
+import React, { Component } from "react";
 import loginPage from "../../src/Images/loginPage.jpg";
 import MainTitle from "./MainTitle";
 import StoreItem from "./StoreItem";
-import DownArrow from '../Images/DownArrow';
-class Landing extends Component{
-	render(){
-		return(
-		<div
-	        className="Login-page"
-	        style={{ backgroundImage: `url(${loginPage})` }}
-	      >
-	        <div className="Login-page-colour login-page-container">
-	        
-	        <div className = "login-page-title">
-	          <MainTitle title={"Reserved"} subTitle={"skip the line"} />
-	        </div>
-	          <button onClick={this.props.toggle} className="browse-yorkdale">Browse Yorkdale           <DownArrow styling={'navigationRightArrow-loginPage'}/>
-</button>
-	          
-	        </div>
-	      </div>
-		)
-	}
+import DownArrow from "../Images/DownArrow";
+import ClothingItemsView from "./ClothingItemsView";
+class Landing extends Component {
+  render() {
+    return (
+      <div
+        className="Login-page"
+        style={{ backgroundImage: `url(${loginPage})` }}
+      >
+        <div className="Login-page-colour login-page-container">
+          <div className="login-page-title">
+            <MainTitle title={"Reserved"} subTitle={"skip the line"} />
+          </div>
+          <button onClick={this.props.toggle} className="browse-yorkdale">
+            Browse Yorkdale
+            <DownArrow styling={"navigationRightArrow-loginPage"} />
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Landing
+export default Landing;
