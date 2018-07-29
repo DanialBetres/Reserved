26a976ef6910ad8b46d2cd8bbcea7ae0936bfdcd
Danial Betres <dbetres@secretmission.io>
2018-07-28 23:43:10 -0400

    store item

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,7 +1,7 @@
 import React, {Component } from 'react'
 import loginPage from "../../src/Images/loginPage.jpg";
 import MainTitle from "./MainTitle";
-
+import StoreItem from "./StoreItem";
 class Landing extends Component{
 	render(){
 		return(
@@ -14,7 +14,7 @@ class Landing extends Component{
 	        <div className = "login-page-title">
 	          <MainTitle title={"Reserved"} subTitle={"skip the line"} />
 	        </div>
-
+						<StoreItem storeName={"Nike"} address={"2nd Floor, Unit 23"} priceScale={[true,true,false]} />
 	          <button onClick={this.props.toggle} className="browse-yorkdale">Browse Yorkdale ></button>
 	          
 	        </div>
--- src/Components/StoreItem.js
+++ src/Components/StoreItem.js
@@ -2,20 +2,23 @@ import React, { Component } from "react";
 
 const StoreItem = ({ storeName, address, priceScale }) => {
   return (
-    <div>
-      <button onClick={() => console.log("goToStorePage")}>
-        <div>
+      <button style={{display:"flex", alignItems:"flex-start"}} onClick={() => console.log("goToStorePage")}>
+        <div className="store-item-container">
           <div className="store-item-details">
             <h3 className="store-item-name">{storeName}</h3>
             <p className="store-item-address">{address}</p>
-            <p className="price-item-scale">
-              {priceScale.array.forEach(element => {
-                <p>{element} </p>;
-              })}
+            <p className="store-item-price-scale">
+              {priceScale.map(element => 
+                <p style={{color: element ? "#4621AD" : "#C4C4C4"}}> $ </p>
+              )}
             </p>
+          </div>
+          <div className="store-item-chevron"style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+            >
           </div>
         </div>
       </button>
-    </div>
   );
 };
+
+export default StoreItem;
