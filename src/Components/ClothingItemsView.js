0a6a80c9ffb208596e6c85e50da96b944c9539db
Danial Betres <dbetres@secretmission.io>
2018-07-29 13:12:50 -0400

    final

--- src/Components/ClothingItemsView.js
+++ src/Components/ClothingItemsView.js
@@ -10,36 +10,36 @@ import ViewSingle from "../Images/ViewSingle";
 import ViewMultiple from "../Images/ViewMultiple";
 import hm from "../Images/hm.jpg";
 import ClothingItemsComponent from "./ClothingItemsComponent";
-import SearchJackets from './SearchJackets'
-import ShowAdd from './ShowAdd'
+import SearchJackets from "./SearchJackets";
+import ShowAdd from "./ShowAdd";
 
 class ClothingItemsView extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       showSearch: false,
-      showAdd : false
-    }
+      showAdd: false
+    };
   }
 
   toggle = () => {
-    this.setState ({ showSearch: !this.state.showSearch})
+    this.setState({ showSearch: !this.state.showSearch });
   };
 
-  add =() => {
-    this.setState({ showAdd: !this.state.showAdd })
-  }
+  add = () => {
+    this.setState({ showAdd: !this.state.showAdd });
+  };
 
-  render(){
+  render() {
     const ShortsData = [
-      { itemImage: shorts1 , name: "Grey Jogger Shorts", price: "$24.99" },
+      { itemImage: shorts1, name: "Grey Jogger Shorts", price: "$24.99" },
       {
-        itemImage: shorts2 ,
+        itemImage: shorts2,
         name: "Light Grey Casual Shorts",
         price: "$29.99"
       },
       {
-        itemImage: short3 ,
+        itemImage: short3,
         name: "Navy Blue Shorts",
         price: "$34.99"
       },
@@ -47,29 +47,32 @@ class ClothingItemsView extends Component {
         itemImage: shorts4,
         name: "Light Denim Shorts",
         price: "$45.99"
-      }, 
+      },
       {
-        itemImage:shorts5,
-        name:"Dark Denim Shorts",
-        price:"$45.99"
+        itemImage: shorts5,
+        name: "Dark Denim Shorts",
+        price: "$45.99"
       }
     ];
-    if(!this.state.showSearch){
+
+    if (!this.state.showSearch) {
       return (
         <div>
-            <ClothingItemsComponent add ={this.add} back={this.props.back} toggle={this.toggle} data={ShortsData} storeImage={hm} searchTitle={"Shorts"}/>
-            {this.state.showAdd && <ShowAdd />}
+          <ClothingItemsComponent
+            add={this.add}
+            back={this.props.back}
+            toggle={this.toggle}
+            data={ShortsData}
+            storeImage={hm}
+            searchTitle={"Shorts"}
+          />
+          {this.state.showAdd && <ShowAdd action={this.add}/>}
         </div>
       );
+    } else {
+      return <SearchJackets back={this.props.back} />;
     }
-    else{
-      return (
-        <SearchJackets back={this.props.back}/>
-      )
-    }
-
   }
-
-};
+}
 
 export default ClothingItemsView;
--- src/Components/ShowAdd.js
+++ src/Components/ShowAdd.js
@@ -1,12 +1,36 @@
 import React from 'react'
-
-const ShowAdd = ({ }) =>{
+import shorts1 from "../Images/shorts1.jpeg"
+const ShowAdd = ({ action }) =>{
 	return(
 
 		<div className = 'addingModal'>
-			<h2>
-					Size
+			<h2 style={{fontFamily:"Montserrat", color:"#222"}}>
+					Hold this item
 			</h2>
+				 <div className="clothingImage" style={{ backgroundImage: `url(${shorts1})` }}>
+				 </div>
+
+			<p style={{fontFamily:"Montserrat", color:"#222", fontWeight:"300"}}> Please select your waist size</p>
+			<select>
+    <option value="">30</option>
+    <option value="dog">32</option>
+    <option value="cat">34</option>
+    <option value="hamster">36</option>
+    <option value="parrot">38</option>
+    <option value="spider">40</option>
+    <option value="goldfish">42</option>
+		</select>
+
+		<p style={{marginTop:"40px",  fontFamily:"Montserrat", color:"#222", fontWeight:"300"}}> How long would you like to hold for?</p>
+			<select style={{marginBottom:"50px", fontFamily:"Montserrat", color:"#222"}}>
+    <option value="">1 hour</option>
+    <option value="dog">2 hours</option>
+    <option value="cat">4 hours</option>
+    <option value="hamster">24 hours</option>
+    <option value="parrot">48 hours</option>
+		</select>
+
+		<div style={{padding:"15px 20px", fontFamily:"Montserrat", color:"#4621AD"}} onClick={action}> Hold this item!</div>
 		</div>
 	)
 }
