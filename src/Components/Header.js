34e713485248e80583992d3ef174e8f51db768c6
Daniel Kim <danman.kim@mail.utoronto.ca>
2018-07-28 23:31:57 -0400

    'dan'

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react'
-
+import DownArrow from '../Images/DownArrow'
 class Header extends Component{
     render(){
         return(
             <div className='headerComp'>
-                
+            	<div className='headerCompTitle'>
+	                <div className='mallLocation'>
+	                	YorkDale
+	                </div>
+	                <DownArrow styling={'navigationDownArrow'}/>
+                </div>
             </div>
         )
     }
