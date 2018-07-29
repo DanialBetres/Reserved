5ec5f45373066cecbd9ff1f56bb1d07c00ebe762
Daniel Kim <danman.kim@mail.utoronto.ca>
2018-07-28 21:31:41 -0400

    'title'

--- src/Components/Homepage/Title.js
+++ src/Components/Homepage/Title.js
@@ -5,47 +5,18 @@ class Title extends Component{
     constructor(props){
         super(props)
         this.state = {
-            mount: false
+            mounted: false
         }
     }
     componentDidMount(){
-        this.setState({ mount: true })
+        this.setState({ mounted: !this.state.mounted })
     }
     render(){
-        if(this.state.mount) {
-            return(
-                <TransitionMotion
-                    styles={
-                        this.state.mount ? [{
-                            key: 'someId',
-
-                            style: {
-                                transform: spring(0),
-                                opacity: spring(1)
-                            }
-                        }] : []
-                    }
-                    willEnter={this.willEnter}
-                    willLeave={this.willLeave}
-                >
-                    {(items) => {
-                        const {key, data, style} = items[0];
-                        const {AnotherComponent, props} = data;
-
-                        return (
-                            <div style={{
-                                transform: `translate(${style.transform}%)`,
-                                opacity: style.opacity
-                            }}>
-                                AHHHHHH
-                            </div>
-                        );
-                    }}
-                </TransitionMotion>
-            
-            )
-        }
-        return null
+        return(
+            <h1 className={`titleHomepage ${this.state.mounted ? '' : ''}`}>
+                Welcome Back, Dan!
+            </h1>
+        )
     }
 }
 
